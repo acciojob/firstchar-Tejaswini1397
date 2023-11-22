a701db987e24d541ed608766afb0c1c3bd775211
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-22 09:23:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function firstChar(text) {
   // your code here
-	 if(text==="") return '';
+	 if(text==="" || text===" ") return '';
 	let str='';
 	str+=text.charAt(0);
 	return str;
   }
+
 
 
 
